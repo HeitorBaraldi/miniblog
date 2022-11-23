f46b22ec6b836c13ed90bfb90c757629776892d7
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-11-23 15:09:41 -0300

    finalizado aula 136

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -27,6 +27,8 @@ export const useAuthentication = () => {
             }
       }
 
+
+
       const createUser = async (data) => {
             checkIfIsCancelled()
             setLoading(true)
@@ -70,6 +72,39 @@ export const useAuthentication = () => {
             
       }
 
+      //logout - signOut
+      const logout = () => {
+            checkIfIsCancelled()
+            signOut(auth)
+      }
+
+      //login - signIn 
+      const login = async(data) => {
+            checkIfIsCancelled()
+
+            setLoading(true)
+            setError(false)
+
+            try {
+                  await signInWithEmailAndPassword(auth, data.email, data.password)
+                  setLoading(false)
+
+            } catch (error) {
+                  let systemErrorMessage 
+
+                  if(error.message.includes("user-not-found")){
+                        systemErrorMessage = "Usuário não encontrado."
+                  } else if (error.message.includes("wrong-password")){
+                        systemErrorMessage = "Senha incorreta."
+                  } else {
+                        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+                  }
+
+                  setError(systemErrorMessage)
+                  setLoading(false)
+            }
+      }
+
       useEffect (()=>{
             return () => setCancelled(true)
       }, []);
@@ -79,5 +114,7 @@ export const useAuthentication = () => {
             createUser,
             error,
             loading,
+            logout,
+            login,
       }
 }
