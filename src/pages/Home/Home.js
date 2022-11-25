b542bd8454d48ec9e3e6f4940fa81846825036df
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-11-25 15:22:12 -0300

    Finalizado projeto MiniBlog

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,15 +2,20 @@ import styles from './Home.module.css'
 
 import {useNavigate, Link} from 'react-router-dom';
 import { useState } from 'react';
-
-
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import PostDetails from '../../components/PostDetails';
 
 const Home = () => {
   const [query, setQuery] = useState("")
-  const [posts] = useState([])
+  const {documents: posts, loading} = useFetchDocuments('posts')
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    if(query) {
+      return navigate(`/search?q=${query}`)
+    }
   }
 
   return (
@@ -20,14 +25,17 @@ const Home = () => {
         <input type="text" placeholder='Ou busque por tags...' onChange={(e)=> setQuery(e.target.value)}/>
         <button className='btn btn-dark'>Pesquisar</button>
       </form>
-      <div>
-        <h1>Posts...</h1>
-        {posts && posts.length === 0 &&(
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
+        {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
           </div>
         )}
+        {posts && posts.map((post) => <PostDetails key={post.id} post={post} />)}
       </div>
     </div>
   )
